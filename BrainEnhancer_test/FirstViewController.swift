e8a20fc645ffd7aa228bd033da22a8777d22b81f
Chris Ward <chriswhward@gmail.com>
2015-07-09 11:21:47 +0100

    first commit

---
+++ BrainEnhancer_test/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BrainEnhancer_test
+//
+//  Created by Brett Romero on 5/31/15.
+//  Copyright (c) 2015 Brett Romero. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()	
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+}
+
